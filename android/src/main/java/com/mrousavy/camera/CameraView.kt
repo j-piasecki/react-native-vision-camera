00b39424bf5ed46c52515f4574f1d9dc15e4e47f
Daniel Hunt <email@danielhunt.info>
2023-10-16 10:26:09 -0400

    fix: Unbind Camera in onDetachedFromWindow and fix minSdkVersion (#1955)
    * fix: camera session is bound and idle on unmount
    
    * fix: abiFilters do not read from gradle.properties
    
    * fix: minSdkVersion does not read from root project
    
    * fix: FOR_HERMES null is truthy

--- android/src/main/java/com/mrousavy/camera/CameraView.kt
+++ android/src/main/java/com/mrousavy/camera/CameraView.kt
@@ -301,6 +301,10 @@ class CameraView(context: Context, private val frameProcessorThread: ExecutorSer
   }
 
   override fun onDetachedFromWindow() {
+    coroutineScope.launch {
+      val cameraProvider = ProcessCameraProvider.getInstance(reactContext).await()
+      cameraProvider.unbindAll()
+    }
     super.onDetachedFromWindow()
     updateLifecycleState()
 
